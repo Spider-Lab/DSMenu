95756b5a38fea8c4858bf6f2ad2821cfbd9cda09
Dieter Baron <dillo@nih.at>
2018-11-08 09:38:19 +0100

    Add license to source files.

--- DSMenu/Classes/DSMWindowController.h
+++ DSMenu/Classes/DSMWindowController.h
@@ -1,10 +1,35 @@
-//
-//  DSMWindowController.h
-//  DSMenu
-//
-//  Created by Dieter Baron on 2014/05/27.
-//  Copyright (c) 2014 Spider Lab. All rights reserved.
-//
+/*
+  DSMWindowController.h -- window controller subclass
+  Copyright (C) 2014 Dieter Baron
+ 
+  This file is part of DSMenu, a utility for Synology Download Station.
+  The authors can be contacted at <dsmenu@spiderlab.at>
+ 
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+  3. The names of the authors may not be used to endorse or promote
+     products derived from this software without specific prior
+     written permission.
+ 
+  THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
+  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import <Cocoa/Cocoa.h>
 
